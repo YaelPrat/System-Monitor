12d3ddb310321dc5e5dac292c91932bd9193b22f
יעל פרת <ylprt@h-MacBook-Pro-sl-yl.local>
2024-03-05 16:26:28 +0200

    The project

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# System-Monitor
+# System Monitor
+## System Monitor is a web application built using Flask, SQLAlchemy, and SocketIO for real-time monitoring of system metrics such as CPU usage, memory usage, and disk usage.
+
+### Features
+Real-time System Metrics: View real-time CPU usage, memory usage, and disk usage metrics in the form of charts.
+Averages Dashboard: Check the average CPU, memory, and disk usage for a selected date.
+Abnormal Results Dashboard: Monitor abnormal system metrics such as CPU, memory, and disk usage exceeding predefined thresholds.
+Interactive Interface: Select a specific date to view historical system metrics.
+Responsive Design: The application interface is designed to be responsive and accessible from various devices.
+
+
+### Usage
+- Home Page: Enter the IP address of the target system and click "Start Monitoring" to begin monitoring system metrics.
+- Charts Dashboard: View real-time CPU usage, memory usage, and disk usage metrics in the form of interactive charts.
+- Averages Dashboard: Select a specific date using the date picker to view average CPU, memory, and disk usage for that date.
+- Abnormal Results Dashboard: Monitor abnormal system metrics such as CPU, memory, and disk usage exceeding predefined thresholds.
+
+### Configuration
+Database: The application uses SQLite as the default database. 
+
+### Dependencies
+- Flask
+- Flask-Migrate
+- Flask-SocketIO
+- Flask-SQLAlchemy
+- Paramiko
+- Chart.js
+- jQuery
+
+### Instructions
+1. Clone the repository to your local machine.
+2. Install the required dependencies using pip:
+    ```
+    pip install -r requirements.txt
+    ```
+3. Ensure you have access to a virtual machine running Kali Linux.
+4. Set up the virtual machine's IP address, username, and password in the application. By default, the username and password are set to "kali" for authentication. The IP address should be provided as input when running the application.
+5. Run the Flask application using the following command:
+    ```
+    python app.py
+    ```
+6. Access the application in your web browser at http://localhost:5006.
+
+### Notes
+- The application expects input in the form of the virtual machine's IP address. Ensure that the IP address is correctly configured to establish an SSH connection.
+- The default username and password for authentication are set to "kali". Modify these credentials as needed based on your virtual machine setup.
+
+
+### Author
+Barak, Dor, Raziel, Yael Prat.
---
+++ app.py
@@ -0,0 +1,199 @@
+from flask import Flask, render_template, jsonify, request
+from flask_migrate import Migrate
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+import paramiko
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+app.config['SECRET_KEY'] = 'secret_key_'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Sample(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Float)
+    type = db.Column(db.String(50))  # Type of the sample ( CPU, memory, RAM)
+    date = db.Column(db.DateTime, default=db.func.current_date())
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'value': self.value,
+            'type': self.type,
+            'date': self.date
+        }
+
+
+# Create a sample
+def add_sample(value, type_):
+    new_sample = Sample(value=value, type=type_)
+    db.session.add(new_sample)
+    db.session.commit()
+
+
+def get_samples_by_date_and_type(target_date, sample_type):
+    # Query the Sample table for samples with the specified date and type
+    with app.app_context():
+        samples = Sample.query.filter_by(date=target_date[:10], type=sample_type).all()
+        return samples
+
+
+def get_abnormal_samples(type_):
+    with app.app_context():
+        if type_ == 'CPU':
+            samples_cpu = Sample.query.filter(Sample.value > 50, Sample.type == 'CPU').all()
+            return samples_cpu
+        if type_ == 'RAM':
+            samples_ram = Sample.query.filter(Sample.value > 70, Sample.type == 'RAM').all()
+            return samples_ram
+        if type_ == 'DISK':
+            samples_disk = Sample.query.filter(Sample.value > 90, Sample.type == 'DISK').all()
+            return samples_disk
+
+
+@app.route('/getAvgs', methods=['GET'])
+def getAvgs():
+    # Get the date parameter from the request query string
+    selected_date = request.args.get('date', default=None)
+
+    if selected_date:
+        cpu_samples = get_samples_by_date_and_type(selected_date, "CPU")
+        ram_samples = get_samples_by_date_and_type(selected_date, "RAM")
+        disk_samples = get_samples_by_date_and_type(selected_date, "DISK")
+
+        # Calculate averages
+        cpu_avg = round(sum(x.value for x in cpu_samples) / len(cpu_samples), 2) if cpu_samples else 0
+        ram_avg = round(sum(x.value for x in ram_samples) / len(ram_samples), 2) if ram_samples else 0
+        disk_avg = round(sum(x.value for x in disk_samples) / len(disk_samples), 2) if disk_samples else 0
+
+        # Prepare data for JSON response
+        data = {
+            'cpu': cpu_avg,
+            'ram': ram_avg,
+            'disk': disk_avg,
+        }
+
+        return jsonify(data)
+    else:
+        # Handle case where no date is provided
+        return jsonify({'error': 'Date parameter is missing'}), 400
+
+
+# Define a global variable for storing historical data
+cpu_history = []
+memory_history = []
+disk_history = []
+db_counter = 0
+ssh_client = paramiko.SSHClient()
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global ssh_client
+    # if ssh_client.get_transport().isAlive():
+    #     ssh_client.close()
+    if request.method == 'POST':
+        global ip
+        ip = request.form.get("ip")
+        host = ip
+        username = 'kali'
+        password = 'kali'
+        # Connect to the VM
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            ssh_client.connect(hostname=host, username=username, password=password, timeout=2)
+        except Exception as e:
+            return render_template("homepage.html", error="The IP invalid")
+
+        return render_template("charts.html")
+    ssh_client.close()
+    return render_template("homepage.html")
+
+
+@app.route('/charts')
+def charts():
+    return render_template("charts.html")
+
+
+@app.route('/averages')
+def averages():
+    return render_template("averages.html")
+
+
+@app.route('/system_metrics')
+def system_metrics():
+    global cpu_history, memory_history, disk_history
+    global db_counter
+
+    # Get current system metrics
+    cpu_percent, disk_usage, memory = get_data(ip)
+
+    # Append current metrics to history
+    cpu_history.append(cpu_percent)
+    memory_history.append(memory)
+    disk_history.append(disk_usage)
+
+    # Keep history length limited to last 50 data points
+    cpu_history = cpu_history[-50:]
+    memory_history = memory_history[-50:]
+
+    # Prepare data for JSON response
+    data = {
+        'cpu_percent': cpu_percent,
+        'memory_percent': memory,
+        'disk_percent': disk_usage,
+        'cpu_history': cpu_history,
+        'memory_history': memory_history
+    }
+
+    db_counter = db_counter + 1
+    if db_counter % 10 == 0:
+        add_sample(float(cpu_percent), "CPU")
+        add_sample(float(memory), "RAM")
+        add_sample(float(disk_usage), "DISK")
+    return jsonify(data)
+
+
+@app.route('/abnormal')
+def abnormal():
+    arg = request.args.get('arg')  # Retrieve the argument from the query parameters
+    samples = get_abnormal_samples(arg)
+    data = {
+        'samples': [sample.serialize() for sample in samples]
+    }
+    return jsonify(data)
+
+
+def get_data(ip):
+    # Define SSH connection parameters
+    # Execute commands on the VM
+    cpu_stdin, cpu_stdout, cpu_stderr = ssh_client.exec_command(
+        "top -bn1 | grep '^%Cpu' |  awk '{print $2 + $4 + $6 + $9 + $11 + $13 + $15}'")
+    mem_stdin, mem_stdout, mem_stderr = ssh_client.exec_command(''' df -h | awk '$NF=="/"{print ($3/$2)*100}' ''')
+    ram_stdin, ram_stdout, ram_stderr = ssh_client.exec_command("free | awk '/^Mem:/{print ($3/$2)*100}'")
+
+    cpu_output, mem_output, ram_output = cpu_stdout.read().decode('utf-8'), mem_stdout.read().decode(
+        'utf-8'), ram_stdout.read().decode('utf-8')
+    # Close the standard input, output, and error streams
+    if cpu_stdin and mem_stdin and ram_stdin:
+        cpu_stdin.close()
+        mem_stdin.close()
+        ram_stdin.close()
+    if cpu_stdout and mem_stdout and ram_stdout:
+        cpu_stdout.close()
+        mem_stdout.close()
+        ram_stdout.close()
+    if cpu_stderr and mem_stderr and ram_stderr:
+        cpu_stderr.close()
+        mem_stderr.close()
+        ram_stderr.close()
+    # Close the SSH connection
+    return cpu_output.strip(), mem_output.strip(), ram_output.strip()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5006, debug=True)
